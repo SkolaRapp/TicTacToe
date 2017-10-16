19c0b322f80b2c8ac35158f335106fa5135fcbc6
M3ttle <nonni@nocando.is>
2017-10-16 15:54:46 +0000

    Second first init with gradle build

---
+++ README.md
@@ -0,0 +1,2 @@
+# TicTacToe
+TicTacToe Written in Java for T-303-HUGB class in Reykjavik University Autumn 2017
---
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -0,0 +1,7 @@
+package is.ru.tictactoe;
+
+public class TicTacToe{
+	public static void main(String[] args){
+		System.out.println("Tikk takk....!");
+	}
+}
