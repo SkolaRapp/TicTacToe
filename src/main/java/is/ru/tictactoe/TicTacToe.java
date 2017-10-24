bede034150dea3b42a8ecf03bfe7e94e469ace07
Gudrun Vald <gudrunv16@ru.is>
2017-10-24 13:11:06 +0000

    first business logic, print board

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,7 +1,31 @@
 package is.ru.tictactoe;
 
-public class TicTacToe{
+public class TicTacToe {
+	
 	public static void main(String[] args){
-		System.out.println("Tikk takk....!!");
+	
+		int[] board = new int[9];
+		int turn = 0;
+
+		//fill the board
+		for(int i = 0; i < board.length; i++) {
+			board[i] = i + 1;
+		}
+
+		System.out.println("TicTacToe");
+
+		//display board
+		int rowCounter = 0;
+		for(int i = 0; i < 9; i++) {
+			System.out.print(board[i] + " ");
+			rowCounter++;
+
+			if(rowCounter % 3 == 0) {
+				System.out.println();
+			}
+		}
+
+		
 	}
+
 }
