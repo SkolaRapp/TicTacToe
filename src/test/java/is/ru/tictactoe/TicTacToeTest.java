eaba63fb957d7fb53c3ab555d88b3b2ca1e7eb9c
unknown <stulli103@gmail.com>
2017-10-24 20:13:24 +0000

    Added a semicolon in the testing file

--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -21,7 +21,7 @@ public class TicTacToeTest {
     	TicTacToe toe = new TicTacToe();
 
     	char [] testBoard = new char[9];
-    	String testString = "X 2 3 \n4 5 6 \n7 8 9 \n"
+    	String testString = "X 2 3 \n4 5 6 \n7 8 9 \n";
     	assertEquals(testString, toe.changeBoard(testBoard));
     }
 }
