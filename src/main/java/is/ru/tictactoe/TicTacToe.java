a6fe5ff0df082260700f525b4895e3e4d36885ba
unknown <stulli103@gmail.com>
2017-10-24 20:14:49 +0000

    Accidentally had a small x instead of a big one

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -43,7 +43,7 @@ public class TicTacToe {
 
 		boardString = printBoard(board);
 		char[] myNewBoard = boardString.toCharArray();
-		myNewBoard[4] = 'x';
+		myNewBoard[4] = 'X';
 		boardString = String.valueOf(myNewBoard);
 
 		return boardString;
