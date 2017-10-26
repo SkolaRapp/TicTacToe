2c90c46dbe2f77e346d3689aef0289330f7141a6
unknown <stulli103@gmail.com>
2017-10-26 18:03:36 +0000

    I still cant get the TemplateEngine to work

--- src/main/java/is/ru/tictactoe/TicTacWeb.java
+++ src/main/java/is/ru/tictactoe/TicTacWeb.java
@@ -1,9 +1,16 @@
 package is.ru.tictactoe;
 
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
 import static spark.Spark.*;
+
 
 public class TicTacWeb {
     public static void main(String[] args) {
+        staticFileLocation("/public");
         /*
         port(getHerokuPort());
         get("/", (req, res) -> {
@@ -15,7 +22,8 @@ public class TicTacWeb {
             );*/
             port(getHerokuPort());
             get("/", (req, res) -> runGame());
-            get("/hello", (request, response) ->
+
+            get("/game", (request, response) ->
         "<!DOCTYPE html>" +
          "<html>" +
          "<head>" +
@@ -34,6 +42,10 @@ public class TicTacWeb {
          "</body>" +
        "</html>"
     );
+
+            //get("/hello", (request, response) -> {
+                //return new ModelAndView(new HashMap(), "templates/hello.vtl");
+            //}, new VelocityTemplateEngine());
         }
 
         static int getHerokuPort() {
@@ -70,7 +82,7 @@ public class TicTacWeb {
 
     private static String printForm(){
         String userForm = "<form action=\"/game/\">"+
-        "Enter number for next value: <input type=\"number\" name=\"uservalue\"  min=\"1\" max=\"9\"><br>" +
+        "Enter number for next value: <input type=\"number\" name=\"uservalue\" min=\"1\" max=\"9\"><br>" +
         "<input type=\"submit\" value=\"Submit\">" +
         "</form>";
         return userForm;
@@ -99,4 +111,26 @@ public class TicTacWeb {
         char[] testBoard = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
         return testBoard;
     } 
+
+    private static String temporaryFunction(){
+        String str = "<!DOCTYPE html>" +
+         "<html>" +
+         "<head>" +
+           "<title>Hello Friend!</title>" +
+           "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
+         "</head>" +
+        "<body>" +
+           "<h1>Hello From Afar</h1>" +
+           "<p>Dear Friend,</p>" +
+           "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
+           "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
+           "<p>But I like programming a lot, so I've got that going for me. </p>" +
+           "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
+           "<p>Cheers,</p>" +
+           "<p>Travel Enthusiast Jane</p>" +
+         "</body>" +
+       "</html>";
+
+       return str;
+    }
 }
