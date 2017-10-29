66761d870fff521f95f448c401940084d28c5fc7
M3ttle <nonni@nocando.is>
2017-10-29 11:26:31 +0000

    files deleted for a mistake

---
+++ src/selenium/java/is/ru/tictactoe/TestTicTacWeb.java
@@ -0,0 +1,32 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TestTicTacWeb extends SeleniumTestWrapper {
+  @Test
+  public void testTitleMatches() {
+    driver.get(baseUrl);
+    assertEquals("CalcWeb", driver.getTitle());
+  }
+
+  @Test
+  public void testInsertNumber() throws Exception {
+    driver.get(baseUrl);
+    /* Remove Thread.sleep... */
+    Thread.sleep(2000);
+
+    WebElement input = driver.findElement(By.id("numberFromForm"));
+    input.sendKeys("1");
+    
+    //Submit form = input.sendKeys(Keys.RETURN)
+    // Or using submit() on the form or any element in the form
+
+    /* Remove Thread.sleep... */
+    Thread.sleep(2000);
+    /* ... finish test! */
+  }
+}
